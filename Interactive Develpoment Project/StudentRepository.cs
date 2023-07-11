1d9e5b234a4c0d37a8f1c932ff58c9ff13067db3
Ajay Persaud <ajaypersaud04@gmail.com>
2023-07-10 21:31:06 -0400

    Create StudentRepository Class - create a list of students - create an "AddStudent" method

---
+++ Interactive Develpoment Project/StudentRepository.cs
@@ -0,0 +1,19 @@
+﻿using Interactive_Develpoment_Project.Logic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interactive_Develpoment_Project
+{
+    public static class StudentRepository
+    {
+        private static List<Student> students = new List<Student>();
+
+        public static void AddStudent(Student student)
+        { 
+            students.Add(student);
+        }
+    }
+}
