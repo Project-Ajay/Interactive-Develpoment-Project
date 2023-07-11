1c21746e9e2580dfe1c0cec3ce969a2db62ce0a3
ykpfly <yangku@shernet.sheridancollege.ca>
2023-07-10 23:16:34 -0400

    Add Course class

--- Interactive Develpoment Project/Logic/Course.cs
+++ Interactive Develpoment Project/Logic/Course.cs
@@ -3,8 +3,53 @@ namespace Interactive_Develpoment_Project.Logic
 {
 	public class Course
 	{
-		public Course()
+		private int _courseId;
+
+		public int CourseId
 		{
+			get { return _courseId; }
+			set
+			{
+				_courseId = value;
+			}
+		}
+
+		private CourseType _courseType;
+		public CourseType CourseType
+		{
+			get { return _courseType; }
+			init
+			{
+				_courseType = value;
+			}
+		}
+
+		private string _courseName;
+		public string CourseName
+		{
+			get { return _courseName; }
+			set
+			{
+				if (string.IsNullOrEmpty(value))
+					throw new Exception("Course Name was not set properly.");
+				_courseName = value;
+						
+			}
+		}
+
+		private string _description;
+		public string Description
+		{
+			get { return _description; }
+			set { _description = value; }
+		}
+
+		public Course(int courseId,CourseType courseType,string courseName,string description)
+		{
+			CourseId = courseId;
+			CourseType = courseType;
+			CourseName = courseName;
+			Description = description;
 		}
 	}
 }
