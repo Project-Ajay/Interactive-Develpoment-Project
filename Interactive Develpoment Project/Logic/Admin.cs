5da3d652ed6ae0f7b12ab1c6ce88f47f547cbf58
ykpfly <yangku@shernet.sheridancollege.ca>
2023-07-11 00:33:20 -0400

    add CourseRepository class

--- Interactive Develpoment Project/Logic/Admin.cs
+++ Interactive Develpoment Project/Logic/Admin.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 namespace Interactive_Develpoment_Project.Logic
 {
+	//admin class handle teacher and admin object
+	//admin can add course for the course list
 	public class Admin
 	{
 		public Admin()
---
+++ Interactive Develpoment Project/Logic/CourseRepository.cs
@@ -0,0 +1,46 @@
+﻿using System;
+namespace Interactive_Develpoment_Project.Logic
+{
+	public static class CourseRepository
+	{
+
+		private static List<Course> _courseList = new List<Course>();
+
+		public static void AddCourse(Course course)
+		{
+			foreach(Course course1 in _courseList)
+			{
+				if (course1.CourseId == course.CourseId)
+					throw new Exception("This course has already in course list.");
+			}
+			_courseList.Add(course);
+		}
+
+		public static Course GetCourseById(int id)
+		{
+			foreach(Course course in _courseList)
+			{
+				if (course.CourseId == id)
+					return course;
+			}
+			return null;
+		}
+
+		public static List<Course> GetCourseByType(CourseType courseType)
+		{
+			List<Course> result = new List<Course>();
+			foreach(Course course in _courseList)
+			{
+				if (course.CourseType == courseType)
+					result.Add(course);
+
+			}
+			return result;
+
+		}
+
+		//read only property to read all course in list
+		public static List<Course> Courses => _courseList;
+	}
+}
+
