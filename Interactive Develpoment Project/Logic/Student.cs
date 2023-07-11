7b30106d71786fda7d889528f90db548cbb73a6f
Ajay Persaud <ajaypersaud04@gmail.com>
2023-07-10 20:39:25 -0400

    Create Field Variables Create Student Name Property (getter and setter(validation))

--- Interactive Develpoment Project/Logic/Student.cs
+++ Interactive Develpoment Project/Logic/Student.cs
@@ -3,9 +3,31 @@ namespace Interactive_Develpoment_Project.Logic
 {
 	public class Student
 	{
-		public Student()
+		private string _studentName;
+		private string _studentEmail;
+		private int _studentId;
+        private DateOnly _dateOfBirth;
+		private string _studentPhoneNumber;
+
+
+		//StudentName property
+		public string StudentName
 		{
+			get
+			{
+				return _studentName;
+			}
+			set 
+			{
+				// if the value is empty an error will be thrown
+				if (string.IsNullOrEmpty(value))
+					throw new Exception("INPUT CAN'T BE EMPTY");
+				_studentName = value;
+			}
 		}
-	}
+
+		//StudentEmail Property 
+
+    }
 }
 
