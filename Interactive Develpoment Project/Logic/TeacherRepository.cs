4510f8b0042f32018eeaeb19235e4d4a476737cc
Arnav Jain <jainarn@shernet.sheridancollege.ca>
2023-07-16 23:37:52 -0400

    Added the teachers list and a "CreateTeacher" method to add new teachers to the list.

--- Interactive Develpoment Project/Logic/TeacherRepository.cs
+++ Interactive Develpoment Project/Logic/TeacherRepository.cs
@@ -3,7 +3,15 @@ namespace Interactive_Develpoment_Project.Logic
 {
 	public static class TeacherRepository
 	{
+        // List of all the teacher that will be in the database
+		private static List<Teacher> _teachers = new List<Teacher>();
 
-	}
+        //method to add more teachers to the database
+        public static void CreateTeacher(Teacher teacher)
+        {
+            _teachers.Add(teacher);
+        }
+
+    }
 }
 
