cba20e26160331a9ca8da4cbc446ab7b561254ba
Arnav Jain <jainarn@shernet.sheridancollege.ca>
2023-07-16 23:57:15 -0400

    Added fields variables to teacher class for different attributes.

--- Interactive Develpoment Project/Logic/Teacher.cs
+++ Interactive Develpoment Project/Logic/Teacher.cs
@@ -3,8 +3,30 @@ namespace Interactive_Develpoment_Project.Logic
 {
 	public class Teacher
 	{
-		
-		public Teacher()
+		private string _teacherFirstName;
+		private string _teacherLastName;
+		private int _teacherId;
+		private string _teacherEmail;
+		private DateOnly _teacherDOB;
+        private int? _teacherPhone;
+
+		private string _teacherAddress;
+		private string _teacherCity;
+		private string _teacherZip;
+		private string _teacherCountry;
+
+        private string _teacherDepartment;
+		private double _teacherSalary;
+		private bool _teacherEmploymentStatus;
+
+        private List<string> _teacherSubjects = new List<string>();
+		private List<Schedule> _teacherSchedule = new List<Schedule>();
+
+
+
+
+        // Constructor
+        public Teacher()
 		{
 		}
 	}
--- Interactive Develpoment Project/Logic/TeacherRepository.cs
+++ Interactive Develpoment Project/Logic/TeacherRepository.cs
@@ -4,9 +4,9 @@ namespace Interactive_Develpoment_Project.Logic
 	public static class TeacherRepository
 	{
         // List of all the teacher that will be in the database
-		private static List<Teacher> _teachers = new List<Teacher>();
+        private static List<Teacher> _teachers = new List<Teacher>();
 
-        //method to add more teachers to the database
+        // Method to add more teachers to the database
         public static void CreateTeacher(Teacher teacher)
         {
             _teachers.Add(teacher);
