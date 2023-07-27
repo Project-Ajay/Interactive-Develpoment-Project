7f754b7a04f422c7661356436ff9dd5f0dd16564
Arnav Jain <jainarn@shernet.sheridancollege.ca>
2023-07-26 23:48:35 -0400

    Added LandingPage to Pages

--- Interactive Develpoment Project/Pages/AdminUI.xaml.cs
+++ Interactive Develpoment Project/Pages/AdminUI.xaml.cs
@@ -1,7 +1,7 @@
 ﻿//Yang's Page
 using Interactive_Develpoment_Project.Logic;
 
-namespace Interactive_Develpoment_Project.UserInterface;
+namespace Interactive_Develpoment_Project.Pages;
 
 public partial class AdminUI : ContentPage
 {
---
+++ Interactive Develpoment Project/Pages/LandingPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace Interactive_Develpoment_Project.Pages;
+
+public partial class LandingPage : ContentPage
+{
+	public LandingPage()
+	{
+		InitializeComponent();
+	}
+}
