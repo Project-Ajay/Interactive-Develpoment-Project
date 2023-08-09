1860c870b59aeec809de40e400eba7926c38a31f
ykpfly <yangku@shernet.sheridancollege.ca>
2023-08-09 16:08:01 -0400

    update exam class. exam class has computed property duration

--- Interactive Develpoment Project/Logic/Exam.cs
+++ Interactive Develpoment Project/Logic/Exam.cs
@@ -3,8 +3,29 @@ namespace Interactive_Develpoment_Project.Logic
 {
 	public class Exam:Assessment
 	{
-		public Exam()
+		private bool _isOpenBook;
+
+		public bool IsOpenBook
 		{
+			get { return _isOpenBook; }
+			set
+			{
+				_isOpenBook = value;
+			}
+		}
+
+		public TimeSpan Duration
+		{
+			get
+			{
+				return base.EndTime-base.StartTime;
+			}
+		}
+
+		public Exam(string title, string decription, DateOnly dateCreated, double grade, TimeSpan startTime, TimeSpan endTime,bool isOpenBook)
+			:base(title,decription,dateCreated,grade,startTime,endTime)
+		{
+			_isOpenBook = isOpenBook;
 		}
 	}
 }
