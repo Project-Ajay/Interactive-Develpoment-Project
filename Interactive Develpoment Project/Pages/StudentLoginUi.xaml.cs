ec23b97dece34445a1217633bf486d52d84c28c6
Ajay Persaud <ajaypersaud04@gmail.com>
2023-08-10 11:17:09 -0400

    Everything is Working!!!!!!!!!!

--- Interactive Develpoment Project/Pages/StudentLoginUi.xaml.cs
+++ Interactive Develpoment Project/Pages/StudentLoginUi.xaml.cs
@@ -4,12 +4,12 @@ using Logic;
 public partial class StudentLoginUi : ContentPage
 {
     //Initialize the list of students (HARD CODED FOR TESTING)
-    //private List<Student> students = new List<Student>
-    //{
-    //    new Student("John Doe", "johndoe@gmail.com", 1000, new DateOnly(2002, 5, 15), "+1 (555) 555-5555", "Pass1234"),
-    //    new Student("Jane Smith", "janesmith@icloud.com", 1001, new DateOnly(2001, 10, 22), "+1 (555) 666-7777", "IcloudPwd1"),
-    //    new Student("Michael Johnson", "michaeljohnson@gmail.com", 1002, new DateOnly(2003, 12, 5), "+1 (555) 444-3333", "P@ssw0rd123")
-    //};
+    private List<Student> students = new List<Student>
+    {
+        new Student("John Doe", "johndoe@gmail.com", 1000, new DateOnly(2002, 5, 15), "+1 (555) 555-5555", "Pass1234"),
+        new Student("Jane Smith", "janesmith@icloud.com", 1001, new DateOnly(2001, 10, 22), "+1 (555) 666-7777", "IcloudPwd1"),
+        new Student("Michael Johnson", "michaeljohnson@gmail.com", 1002, new DateOnly(2003, 12, 5), "+1 (555) 444-3333", "P@ssw0rd123")
+    };
     
     public StudentLoginUi()
 	{
@@ -21,35 +21,31 @@ public partial class StudentLoginUi : ContentPage
     {
 
 
-
-
-
 		string studentId = StudentIDEntry.Text;
 		string studentPassword = StudentPasswordEntry.Text;
 
         bool isStudentFound = false;
 
-        //Arnav: Fix the code before pushing it
-        //foreach (Student student in students)
-        //{
+        foreach (Student student in students)
+        {
 
-        //    if (student.StudentId.ToString() == studentId && student.Password == studentPassword)
-        //    {
-        //        isStudentFound = true;
-        //    }
-        //}
-        //if (isStudentFound)
-        //{
+            if (student.StudentId.ToString() == studentId && student.Password == studentPassword)
+            {
+                isStudentFound = true;
+            }
+        }
+        if (isStudentFound)
+        {
 
-        //    StudentInfoUi studentInfoUi = new StudentInfoUi();
-        //    await Navigation.PushAsync(studentInfoUi);
-        //}
-        //else
-        //{
-        //    await DisplayAlert("Error", "Invalid student ID or password. Please try again.", "OK");
-        //    StudentIDEntry.Text = "";
-        //    StudentPasswordEntry.Text = "";
+            StudentInfoUi studentInfoUi = new StudentInfoUi();
+            await Navigation.PushAsync(studentInfoUi);
+        }
+        else
+        {
+            await DisplayAlert("Error", "Invalid student ID or password. Please try again.", "OK");
+            StudentIDEntry.Text = "";
+            StudentPasswordEntry.Text = "";
 
-        //}
+        }
     }
 }
