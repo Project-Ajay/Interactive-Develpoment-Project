6b3a81aa2a7f20c8f5f3890c3b601b35438b6610
Arnav Jain <jainarn@shernet.sheridancollege.ca>
2023-08-10 01:46:42 -0400

    Added 2 Find methods in teacherrepository class and learned how to check strings while ignoring case.

--- Interactive Develpoment Project/Logic/TeacherRepository.cs
+++ Interactive Develpoment Project/Logic/TeacherRepository.cs
@@ -6,12 +6,36 @@ namespace Interactive_Develpoment_Project.Logic
         // List of all the teacher that will be in the database
         private List<Teacher> _teachers = new List<Teacher>();
 
+        //property to access the teachers list outside of this class
         public List<Teacher> Teachers { get { return _teachers; } }
 
         // Method to add more teachers to the database
         public void CreateTeacher(Teacher teacher)
         {
             _teachers.Add(teacher);
+        }
+
+        public Teacher FindTeacherByName(string name)
+        {
+            foreach(Teacher teacher in _teachers)
+            {
+                //compares the name provided and all the names in the database
+                //learned how to ignore case while checking string here: https://learn.microsoft.com/en-us/dotnet/csharp/how-to/compare-strings
+                if (string.Equals(name, teacher.TeacherName, StringComparison.OrdinalIgnoreCase))
+                    return teacher;
+            }
+            return null;
+        }
+
+        public Teacher FindTeacherById(string id)
+        {
+            foreach (Teacher teacher in _teachers)
+            {
+                //compares the teacher's id to find a teacher in the database
+                if(string.Equals(id, teacher.TeacherId, StringComparison.OrdinalIgnoreCase))
+                    return teacher;
+            }
+            return null;
         }
 
     }
