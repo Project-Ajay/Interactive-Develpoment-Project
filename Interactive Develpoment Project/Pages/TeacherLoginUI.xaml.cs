9e5494f1717a522ffbbbb8d6b4de775311ec8606
Arnav Jain <jainarn@shernet.sheridancollege.ca>
2023-08-11 01:17:37 -0400

    Updated teacherUI the password entry now has an option to show or hide password.

--- Interactive Develpoment Project/Pages/TeacherLoginUI.xaml.cs
+++ Interactive Develpoment Project/Pages/TeacherLoginUI.xaml.cs
@@ -14,5 +14,18 @@ public partial class TeacherLoginUI : ContentPage
 
     private void showPassword_clicked(object sender, EventArgs e)
     {
+        //If the image button is clicked it will change the image and show/hide the text.
+        //found the property "IsPassword" here: https://learn.microsoft.com/en-us/dotnet/api/microsoft.maui.controls.entry.ispassword?view=net-maui-7.0.
+        if (TeacherPasswordEntry.IsPassword==true)
+        {
+            TeacherPasswordEntry.IsPassword = false;
+            showPasswordImageBtn.Source = "hidepassword.png";
+            
+        }
+        else if (TeacherPasswordEntry.IsPassword == false)
+        {
+            TeacherPasswordEntry.IsPassword=true;
+            showPasswordImageBtn.Source = "showpassword.png";
+        }
     }
 }
